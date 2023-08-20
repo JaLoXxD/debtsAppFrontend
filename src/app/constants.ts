b126335230c3f41dafeb96cb72e98a6e3f47b20b
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-08-20 15:42:25 -0500

    [ADD] constants

---
+++ src/app/constants.ts
@@ -0,0 +1,39 @@
+export const constants = {
+  DEFAULT_LANG: "es",
+  LANG_OPTIONS: [
+    {
+        LANG: "es",
+        ICON: "assets/img/lang/es.png",
+        LABEL: "lang.es"
+    },
+    {
+        LANG: "en",
+        ICON: "assets/img/lang/en.png",
+        LABEL: "lang.en"
+    }
+  ],
+  ALERTS: {
+    TYPES: {
+      SUCCESS: {
+        label: "alert.success",
+        icon: "fa-circle-check",
+        color: "#28a745"
+      },
+      DANGER: {
+        label: "alert.danger",
+        icon: "fa-circle-exclamation",
+        color: "#dc3545"
+      },
+      WARNING: {
+        label: "alert.warning",
+        icon: "fa-triangle-exclamation",
+        color: "#ffc107"
+      },
+      INFO: {
+        label: "alert.info",
+        icon: "fa-info-circle",
+        color: "#17a2b8"
+      }
+    },
+  }
+};
