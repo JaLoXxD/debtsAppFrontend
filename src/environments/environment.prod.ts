b15b0919ad6742ad29b5401db729b3b04998ab5c
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-08-20 15:40:20 -0500

    [ADD] env vars

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiAuthURL: 'http://localhost:8081/api/v1/auth',
+  apiURL: 'http://localhost:8081/api/v1',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiAuthURL: 'http://localhost:8081/api/v1/auth',
+  apiURL: 'http://localhost:8081/api/v1',
+};
