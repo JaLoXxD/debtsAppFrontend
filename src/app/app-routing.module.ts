fdd604d6ba3ad99475dd8f97535535cad28404d1
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-08-20 15:49:00 -0500

    [ADD] routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginViewComponent } from "./views/login-view/login-view.component";
+import { HomeViewComponent } from "./views/home-view/home-view.component";
+import { authGuard } from "./services";
+import { RecoverPasswordViewComponent } from "./views/recover-password-view/recover-password-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeViewComponent, canActivate: [authGuard] },
+  { path: "auth/login", component: LoginViewComponent },
+  { path: "auth/recover-password", component: RecoverPasswordViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
