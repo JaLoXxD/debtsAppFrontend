373c91097c6342c757bcafa700471a3d430a8432
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-09-01 12:26:00 -0500

    [UPD] auth guard if user must reset password

--- src/app/services/auth/auth.guard.ts
+++ src/app/services/auth/auth.guard.ts
@@ -1,12 +1,13 @@
 import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from "@angular/router";
 import { AuthService } from "./auth.service";
+import { UserService } from "../user/user.service";
 
 export const authGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
-  return inject(AuthService).isLoggedIn()
+  return inject(AuthService).isLoggedIn() && !inject(UserService).userInfo?.resetPassword
     ? true
-    : inject(Router).createUrlTree(['/auth/login']);
+    : inject(UserService).userInfo?.resetPassword ? inject(Router).createUrlTree(['/update-password']) : inject(Router).createUrlTree(['/auth/login']);
 };
