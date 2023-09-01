2038334a5244b2fec7f103a010b360a413790207
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-09-01 12:31:42 -0500

    [UPD] index services

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,4 +1,5 @@
 export * from './translation/translation.service';
 export * from './auth/auth.service';
 export * from './auth/auth.guard';
+export * from './user/user.service';
 export * from './alert/alert.service';
