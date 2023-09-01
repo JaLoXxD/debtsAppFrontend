3debb25da367f086cbbcf12614a7531ac0b515ad
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-09-01 12:27:11 -0500

    [ADD] accept language header in all requests

--- src/app/services/auth/auth.interceptor.service.ts
+++ src/app/services/auth/auth.interceptor.service.ts
@@ -6,21 +6,28 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { TranslationService } from '../translation/translation.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private _translationService: TranslationService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem('token');
-    if (token) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      });
+    const token = sessionStorage.getItem('token');
+    const currentPrefix = this._translationService.getCurrentPrefix();
+    const headers: Record<string, string> = {
+      'Accept-Language': currentPrefix,
     }
+    if (token) {
+      headers['Authorization'] = `Bearer ${token}`;
+    }
+    request = request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+        ...headers
+      }
+    });
     return next.handle(request);
   }
 }
