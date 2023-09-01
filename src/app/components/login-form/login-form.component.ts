71364cee6a21e6871d15922cf7b3a696adb97056
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-09-01 12:24:24 -0500

    [UPD] login form component

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,19 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
 import { LoginModel, LoginResponseModel } from "src/app/models";
-import { AlertService, AuthService } from "src/app/services";
+import { AlertService, AuthService, UserService } from "src/app/services";
+import { BaseComponent } from "src/app/shared/base/base.component";
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent {
+export class LoginFormComponent extends BaseComponent{
+  @ViewChild('f') loginForm!: NgForm;
+
   userInfo!: LoginModel;
   isVisiblePassword: boolean = false;
   isLoading: boolean = false;
 
-  constructor(private _authService: AuthService, private _alertService: AlertService, private _router: Router) {
+  constructor(private _authService: AuthService, private _alertService: AlertService, private _userService: UserService, private _router: Router) {
+    super();
     this._initModel();
   }
 
@@ -25,18 +30,28 @@ export class LoginFormComponent {
     this.isLoading = true;
     this._authService.login(this.userInfo).subscribe({
       next: (resp) => {
-        console.log('Login success');
-        console.log(resp);
-        this._router.navigate(['/']);
         this._setLocalStorageValues(resp);
+        this._getUserInfo();
       },
       error: (err) => {
-        console.log('Login failed');
-        this._alertService.setAlerts(this._alertService.mapErrors(err.error));
+        this._alertService.setAlerts(this._alertService.mapErrors(this.setErrors(err)));
       },
     }).add(() => {
       this.isLoading = false;
     });
+  }
+
+  private _getUserInfo(): void {
+    this._userService.getUserInfo().subscribe(
+      (resp) => {
+        this._userService.userInfo = resp.user;
+        let  redirectUrl = "/";
+        if(resp.user.resetPassword) {
+          redirectUrl = "/update-password";
+        }
+        this._router.navigate([redirectUrl]);
+      }
+    );
   }
 
   private _setLocalStorageValues(loginResp: LoginResponseModel): void {
