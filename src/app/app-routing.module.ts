6e490f98576344d90300cfade8b7ba265fb30773
JorgeOrlando512 <jorgino91.jh@gmail.com>
2023-09-01 12:33:23 -0500

    [ADD] new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { LoginViewComponent } from "./views/login-view/login-view.component";
 import { HomeViewComponent } from "./views/home-view/home-view.component";
 import { authGuard } from "./services";
 import { RecoverPasswordViewComponent } from "./views/recover-password-view/recover-password-view.component";
+import { RegisterViewComponent } from "./views/register-view/register-view.component";
+import { UpdatePasswordViewComponent } from "./views/update-password-view/update-password-view.component";
 
 const routes: Routes = [
   { path: '', component: HomeViewComponent, canActivate: [authGuard] },
   { path: "auth/login", component: LoginViewComponent },
   { path: "auth/recover-password", component: RecoverPasswordViewComponent },
+  { path: "auth/register", component: RegisterViewComponent },
+  { path: "update-password", component: UpdatePasswordViewComponent},
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
